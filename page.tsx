41073a12e37cd11f5dc02aa125637a06153692bb
whooletusstudy <bhavaanisiya41011@gmail.com>
2025-04-14 22:51:58 +0530

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>Welcome to StudyGenie</h1>
+      <p>Your AI-powered study assistant is here!</p>
+    </div>
+  );
+}
