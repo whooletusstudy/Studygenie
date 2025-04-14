b72c1135983fcd4b3bbc46236db6d3bb0db72b2c
whooletusstudy <bhavaanisiya41011@gmail.com>
2025-04-14 22:48:14 +0530

    Delete page.tsx

--- page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontSize: '24px' }}>
-      <h1>Welcome to StudyGenie!</h1>
-    </main>
-  );
-}
